edc36b3b51e091cbcb9192bb914b043a5a14e76b
Daniella Mesquita <mesquitadaniella@proton.me>
2024-06-15 00:28:36 -0300

    complement previous commit

--- src/components/Pages/Extra/Mintables/MintablesItem.js
+++ src/components/Pages/Extra/Mintables/MintablesItem.js
@@ -24,15 +24,16 @@ const MintablesItem = () => {
     <div class="col-md">
       <img draggable="false" class="cover" src={`${process.env.PUBLIC_URL}/${mintable.image}`} alt={mintable.name} />
           <a title="Click to Zoom/open img on new tab" href={`${process.env.PUBLIC_URL}/${mintable.image}`} target="_blank" rel="noopener noreferrer"><i class="fas fa-magnifying-glass-plus"></i></a>
-          <a title="Source post at Lens' Orb.ac" href={mintable.Orb} target="_blank" rel="noopener noreferrer"><i><img src="../../img/social/button/orb.svg" width="16px" height="16px"></img></i></a>
+          <a title="IPFS" href={mintable.ipfs} target="_blank" rel="noopener noreferrer"><i><img src="../../img/ui/button/ipfs.svg" width="16px" height="16px"></img></i></a>
       </div>
       <div class="col-md mintable-details">
       <h1>{mintable.name}</h1>
       <div className="links">
         <a href={mintable.OpenSea} target="_blank" rel="noopener noreferrer"><button className="btn btn-primary"><img src="../../img/social/button/opensea.svg" width="32px" height="32px"></img> OpenSea</button></a>
+        <a href={mintable.Orb} target="_blank" rel="noopener noreferrer"><button className="btn btn-primary"><img src="../../img/social/button/orb.svg" width="32px" height="32px"></img> Mint on Orb</button></a>
+        <br/>
         <a href={mintable.BlockchainScan} target="_blank" rel="noopener noreferrer"><button className="btn btn-primary"><img src="../../img/social/button/etherscan.svg" width="32px" height="32px"></img> BlockchainScan</button></a>
         <a href={mintable.Arweave} target="_blank" rel="noopener noreferrer"><button className="btn btn-primary"><img src="../../img/social/button/arweave.svg" width="32px" height="32px"></img> Arweave</button></a>
-        <a href={mintable.ipfs} target="_blank" rel="noopener noreferrer"><button className="btn btn-primary"><img src="../../img/ui/ipfs-c.svg" width="32px" height="32px"></img> IPFS</button></a>
       </div>
       </div>
       </div>
